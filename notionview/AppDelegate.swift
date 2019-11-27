d156550193d89f101121ccaabd404adc04650c6f
ljnjiannan <ljnjiannan@163.com>
2019-11-27 09:02:41 +0800

    Initial Commit

---
+++ notionview/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  notionview
+//
+//  Created by jiannan liu on 2019/11/27.
+//  Copyright © 2019 jiannan liu. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ notionview/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  notionview
+//
+//  Created by jiannan liu on 2019/11/27.
+//  Copyright © 2019 jiannan liu. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
